e6eee635a59bdbc41dc818c1366d6eb21a9031e6
Ailen Ojeda <ailen.ojeda21@gmail.com>
2024-10-14 21:43:10 -0300

    Guardar y eliminar categoria

--- Backend/src/models/categoria.model.js
+++ Backend/src/models/categoria.model.js
@@ -1,49 +1,70 @@
 const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+
 class Categoria {
-  constructor(nombre) {
-    this.idCategoria = uuidv4();
+  constructor(nombre, idCategoria = uuidv4()) {
+    this.idCategoria = idCategoria;
     this.nombre = nombre;
   }
 
   static fromJSONtoObjectCategoria(dataCategoria) {
-    let nuevoCategoria = new Categoria(
-      dataCategoria.idCategoria,
-      dataCategoria.nombre
-    );
-
-    return nuevoCategoria;
+    return new Categoria(dataCategoria.nombre, dataCategoria.idCategoria);
   }
 }
+
 function guardarCategoria(dataBody) {
   const CategoriaNew = Categoria.fromJSONtoObjectCategoria(dataBody);
+  let categoriasRegistradas = obtenerObjetosBD("../backend/src/db/categorias.txt");
 
-  let CategoriasRegistradas = obtenerObjetosBD("../db/categorias.txt");
-
-  CategoriasRegistradas.push(CategoriaNew);
-  escribirObjetosBD("../db/categorias.txt", CategoriaNew);
+  categoriasRegistradas.push(CategoriaNew);
+  escribirObjetosBD("../backend/src/db/categorias.txt", categoriasRegistradas);
 
   return CategoriaNew;
 }
 
-//Metodos para consultar bd. path-> ruta del archivo .txt--------------------------------------------------------
 function obtenerObjetosBD(path) {
   let stringDeObjetos = fs.readFileSync(path, "utf-8");
   if (!stringDeObjetos.trim()) {
-    console.log("El archivo está vacío.");
-    return []; // Retorna un array vacío cuando aun no se cargan productos
+    return [];
   }
-  let objetosEnBD = JSON.parse(stringDeObjetos);
-
-  return objetosEnBD;
+  return JSON.parse(stringDeObjetos);
 }
 
-function escribirObjetosBD(path, objeto) {
-  if (!path || !objeto) {
-    return false;
-  }
-  fs.writeFileSync(path, JSON.stringify(objeto));
-  return true;
+function escribirObjetosBD(path, objetos) {
+  fs.writeFileSync(path, JSON.stringify(objetos, null, 2));
 }
+
+
+
+
+
+
+
+
+
+
+
+function eliminarCategoria(idCategoria) {
+  try {
+    let categoriasRegistradas = obtenerObjetosBD(
+      "../backend/src/db/categorias.txt"
+    );
+
+    const categoriasActualizadas = categoriasRegistradas.filter(
+      (categoria) => categoria.idCategoria !== idCategoria // Asegúrate de que esto coincida con la propiedad de tu objeto
+    );
+
+    escribirObjetosBD("../backend/src/db/categorias.txt", categoriasActualizadas); // Asegúrate de pasar la ruta correcta
+    return categoriasActualizadas; // Devolver las categorías actualizadas para uso posterior
+  } catch (error) {
+    console.error(error); // Agregar un log del error
+    return null; // En caso de error, retornar null para manejarlo en el controlador
+  }
+}
+
+
+
 module.exports = {
-  guardarCategoria,
+  guardarCategoria, // Exportamos guardarCategoria
+  eliminarCategoria,
 };
